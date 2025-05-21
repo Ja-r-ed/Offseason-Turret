d95f4ddd9fbbfb14d3fb332769a60d69d5489b66
ICWorkshop <icroboticsfll@gmail.com>
2025-05-21 19:59:35 +1000

    Shooter and Hood semi completed

--- src/main/cpp/subsystems/SubShooter.cpp
+++ src/main/cpp/subsystems/SubShooter.cpp
@@ -3,12 +3,40 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/SubShooter.h"
+#include "frc/smartdashboard/SmartDashboard.h"
 
 SubShooter::SubShooter() {
+    frc::SmartDashboard::PutData("Shooter/Motor1", &_shooterMotor1);
+    frc::SmartDashboard::PutData("Shooter/Motor2", &_shooterMotor2);
 
+    _shooterMotor1Config.encoder.PositionConversionFactor(1/GEAR_RATIO);
+    _shooterMotor1Config.encoder.VelocityConversionFactor(GEAR_RATIO/1);
+    _shooterMotor1Config.closedLoop.Pid(P,I,D, rev::spark::ClosedLoopSlot::kSlot1);
+    _shooterMotor1Config.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kCoast);
+    auto err1 = _shooterMotor1.AdjustConfig(_shooterMotor1Config);
+    frc::SmartDashboard::PutNumber("Shooter/config set err1", (int)err1);
+
+    _shooterMotor2Config.Follow(canid::SHOOTER_MOTOR_1, true);
+    auto err2 = _shooterMotor1.AdjustConfig(_shooterMotor1Config);
+    frc::SmartDashboard::PutNumber("Shooter/config set err2", (int)err2);
+    
 }
 
 // This method will be called once per scheduler run
 void SubShooter::Periodic() {
 
+}
+
+frc2::CommandPtr SubShooter::SetShooterSpeed(units::turns_per_second_t speed) {
+    return RunOnce([this, speed] {_shooterMotor1.SetVelocityTarget(speed);});
+}
+
+
+frc2::CommandPtr SubShooter::SpinUpShooter() {
+    return RunOnce([this] {_shooterMotor1.SetVelocityTarget(SHOOTER_SPEED);});
+}
+
+
+frc2::CommandPtr SubShooter::StopShooter() {
+    return RunOnce([this] {_shooterMotor1.SetVelocityTarget(0_tps);});
 }
