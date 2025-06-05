aba08d1e41a7b6687ef3650c6470f415793d9753
Jared PC <jared.jj.jin@gmail.com>
2025-06-05 16:15:35 +1000

    created turret motor and basic turret CRT angle function

--- src/main/include/subsystems/SubTurret.h
+++ src/main/include/subsystems/SubTurret.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include "utilities/ICSparkMax.h"
+#include "Constants.h"
 
 class SubTurret : public frc2::SubsystemBase {
  public:
@@ -13,12 +15,27 @@ class SubTurret : public frc2::SubsystemBase {
     static SubTurret inst;
     return inst;
   }
+
+  double GetTurretAngle(double encoder1, double encoder2);
+
+
+
   /**
    * Will be called periodically whenever the CommandScheduler runs.
    */
   void Periodic() override;
 
  private:
+  ICSparkMax _turretMotor{canid::TURRET_MOTOR, 30_A};
+  rev::spark::SparkBaseConfig _turretMotorConfig;
+
+  double P = 0;
+  double I = 0;
+  double D = 0;
+  
+  static constexpr double ENCODER1_RATIO = 20/115;
+  static constexpr double ENCODER2_RATIO = 22/115;
+  static constexpr double GEAR_RATIO = 1/46;
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
